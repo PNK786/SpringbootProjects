6be1c5184e3d8e94bfb310291276d1db3bb5f9fd
AfrozKhan <pathanafrozkhan786@gmail.com>
2025-04-19 19:38:58 +0530

    rabbitmq initial commit

---
+++ SpringbootWithRabbitMq/src/main/java/com/Rabbitmq/Springboot/SpringbootWithRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.Rabbitmq.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithRabbitMqApplication.class, args);
+	}
+
+}
---
+++ SpringbootWithRabbitMq/src/test/java/com/Rabbitmq/Springboot/SpringbootWithRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rabbitmq.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
